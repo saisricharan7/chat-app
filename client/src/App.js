3cc99dfd28401fd63dbec02a7893208c7c5decad
saisricharan <saisricharan.7@gmail.com>
2023-09-27 22:20:38 +0530

    Revert "second commit"
    This reverts commit 90476dd698844aa4ba33a4d0b27161e11027aaa4.

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,25 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import SetAvatar from "./components/SetAvatar";
-import Chat from "./pages/Chat";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-export default function App() {
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/" element={<Chat />} />
-      </Routes>
-    </BrowserRouter>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
+
+export default App;
