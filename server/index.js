c98cacb36c640d73b03f4910a4528aac699ddc9d
saisricharan <saisricharan.7@gmail.com>
2023-09-27 23:43:27 +0530

    packages installed and server running

---
+++ server/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+
+const app = express();
+const socket = require("socket.io");
+require("dotenv").config();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DB Connetion Successfull");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
