a614debde8e53ff35ded410ae8ac55073625f94c
saisricharan <saisricharan.7@gmail.com>
2023-11-30 22:01:32 +0530

    setavatar route added

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -40,4 +40,25 @@ module.exports.login = async (req, res, next) => {
       next(ex);
     }
   };
+
+  module.exports.setAvatar = async(req,res,next)=>{
+    try{
+      const userId=req.params.id;
+      const avatarImage=req.body.image;
+      const userData = await User.findByIdAndUpdate(
+        userId,
+        {
+          isAvatarImageSet: true,
+          avatarImage,
+        },
+        { new: true }
+      );
+      return res.json({
+        isSet: userData.isAvatarImageSet,
+        image: userData.avatarImage,
+      });
+    }catch(ex){
+      next(ex)
+    }
+  }
   
