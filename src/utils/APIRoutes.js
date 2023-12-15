4379a92254c22d221abbc13a1c172ae1fbd2a227
saisricharan <saisricharan.7@gmail.com>
2023-12-15 20:58:24 +0530

    render link added

--- src/utils/APIRoutes.js
+++ src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = "http://localhost:5000";
+export const host = "https://chatapp-be-0v6k.onrender.com";
 export const loginRoute = `${host}/api/auth/login`;
 export const registerRoute = `${host}/api/auth/register`;
 export const setAvatarRoute = `${host}/api/auth/setAvatar`;
